74ba4a4d96e732212553fb99388f56592c9ae6d5
MasahiroToyooka <ehara2481@gmail.com>
2019-07-26 16:06:48 +0900

    Initial Commit

---
+++ SlideMenu_storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenu_storyboard
+//
+//  Created by 豊岡正紘 on 2019/07/26.
+//  Copyright © 2019 Masahiro Toyooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
